a9a0c2a977f8f1dfc7c9d3da4e386b8e50c6eb1a
Elizabeth Venner <elizabethvenner@googlemail.com>
2016-09-15 21:21:46 +0000

    Add method for adding students interactively

--- directory.rb
+++ directory.rb
@@ -1,5 +1,5 @@
 #let's put all students into an array
-students = [
+=begin students = [
     {name: "Dr. Hannibal Lecter", cohort: :november},
     {name: "Darth Vader", cohort: :november},
     {name: "Nurse Ratched", cohort: :november},
@@ -12,7 +12,27 @@ students = [
     {name: "Joffrey Baratheon", cohort: :november},
     {name: "Norman Bates", cohort: :november}   
     ]
-    
+=end
+
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+    #create an empty array
+    students = []
+    #get the first name
+    name = gets.chomp
+    #while the name is not empty, repeat this code
+    while !name.empty? do
+        #add the student hash to the array
+        students << {name: name, cohort: :november}
+        puts "Now we have #{students.count} students"
+        #get another name from the user
+        name = gets.chomp
+    end
+    #return the array of students
+    students
+end
+
 def print_header
     puts "The students of Villains Academy"
     puts "_______________"
@@ -28,6 +48,7 @@ def print_footer(names)
     puts "Overall, we have #{names.count} great students"
 end
 #nothing happens until we call the methods
+students = input_students   
 print_header
 print(students)
 print_footer(students)
