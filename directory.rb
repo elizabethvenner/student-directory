eeeef4a35315f7365eac3e7f9fb8d9c336c9a949
Elizabeth Venner <elizabethvenner@googlemail.com>
2016-09-17 18:12:26 +0000

    9. Adding an interactive menu

--- directory.rb
+++ directory.rb
@@ -1,18 +1,27 @@
-#let's put all students into an array
-=begin students = [
-    {name: "Dr. Hannibal Lecter", cohort: :november},
-    {name: "Darth Vader", cohort: :november},
-    {name: "Nurse Ratched", cohort: :november},
-    {name: "Michael Corleone", cohort: :november},
-    {name: "Alex DeLarge", cohort: :november},
-    {name: "The Wicked Witch of the West", cohort: :november},
-    {name: "Terminator", cohort: :november},
-    {name: "Freddy Krueger", cohort: :november},
-    {name: "The Joker", cohort: :november},
-    {name: "Joffrey Baratheon", cohort: :november},
-    {name: "Norman Bates", cohort: :november}   
-    ]
-=end
+def interactive_menu
+    students = []   
+    loop do
+        #1. print the menu and ask the user what to do
+        puts "1. Input the students"
+        puts "2. Show the students"
+        puts "9. Exit"
+        #2. read the input and save it into a variable
+        selection = gets.chomp
+        #3. do what the user has asked
+        case selection
+        when "1"
+            students = input_students
+        when "2"
+            print_header
+            print(students)
+            print_footer
+        when "9"
+            exit # this will cause the program to terminate
+        else
+            puts "I don't know what you meant, try again"
+        end
+    end
+end
 
 def input_students
     puts "Please enter the names of the students"
@@ -84,9 +93,29 @@ def print_footer(names)
     end
 end
 #nothing happens until we call the methods
+interactive_menu
+
 students = input_students   
 print_header
 print(students)
 print_footer(students)
 
 
+
+
+
+#let's put all students into an array
+=begin students = [
+    {name: "Dr. Hannibal Lecter", cohort: :november},
+    {name: "Darth Vader", cohort: :november},
+    {name: "Nurse Ratched", cohort: :november},
+    {name: "Michael Corleone", cohort: :november},
+    {name: "Alex DeLarge", cohort: :november},
+    {name: "The Wicked Witch of the West", cohort: :november},
+    {name: "Terminator", cohort: :november},
+    {name: "Freddy Krueger", cohort: :november},
+    {name: "The Joker", cohort: :november},
+    {name: "Joffrey Baratheon", cohort: :november},
+    {name: "Norman Bates", cohort: :november}   
+    ]
+=end
