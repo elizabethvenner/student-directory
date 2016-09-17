f5e30336c87ea5292e94c379cf1ab6b8c99479e2
Elizabeth Venner <elizabethvenner@googlemail.com>
2016-09-17 18:45:43 +0000

    Exercise 10 shorten interactive_menu by creating methods print_menu, show_students, process and making students an instance variable

--- directory.rb
+++ directory.rb
@@ -1,27 +1,4 @@
-def interactive_menu
-    students = []   
-    loop do
-        #1. print the menu and ask the user what to do
-        puts "1. Input the students"
-        puts "2. Show the students"
-        puts "9. Exit"
-        #2. read the input and save it into a variable
-        selection = gets.chomp
-        #3. do what the user has asked
-        case selection
-        when "1"
-            students = input_students
-        when "2"
-            print_header
-            print(students)
-            print_footer
-        when "9"
-            exit # this will cause the program to terminate
-        else
-            puts "I don't know what you meant, try again"
-        end
-    end
-end
+@students = []  
 
 def input_students
     puts "Please enter the names of the students"
@@ -61,8 +38,41 @@ def input_students
         #get another name from the user
         name = gets.chomp
     end
-    #return the array of students
-    students
+end
+
+def interactive_menu
+    loop do
+        #1. print the menu and ask the user what to do
+        print_menu
+        #2. read the input and save it into a variable
+        process(gets.chomp)
+        #3. do what the user has asked
+    end
+end
+
+def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+end
+
+def show_students
+    print_header
+    print_students_list(@students)
+    print_footer(@students)
+end
+
+def process (selection)
+    case selection
+        when "1"
+            @students = input_students
+        when "2"
+            show_students
+        when "9"
+            exit # this will cause the program to terminate
+        else
+            puts "I don't know what you meant, try again"
+    end
 end
 
 def print_header
@@ -70,9 +80,9 @@ def print_header
     puts "_______________".center(20)
 end
 
-def print(students)
+def print_students_list(students)
     cohorts = [:January, :February, :March, :April, :May, :June, :July, :August, :September, :October, :November, :December]
-    if students.count >= 1
+    if students.count >= 1 # this is throwing an error - investigate later
         counter = 0
         cohorts.each do |cohort|
             students.each do |student|
@@ -94,14 +104,6 @@ def print_footer(names)
 end
 #nothing happens until we call the methods
 interactive_menu
-
-students = input_students   
-print_header
-print(students)
-print_footer(students)
-
-
-
 
 
 #let's put all students into an array
